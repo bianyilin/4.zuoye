0b650654e47b36bb59d2e03730b3efdacadea684
bianyilin <49523527+bianyilin@users.noreply.github.com>
2019-05-04 11:33:09 +0800

    Add files via upload

---
+++ zy1.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: zy1.c
+	> Author: bianyilin
+	> Mail: 732406982@qq.com 
+	> Created Time: 2019年04月21日 星期日 10时56分25秒
+ ************************************************************************/
+
+#include <stdio.h>
+
+int main() {
+    printf("\033[40;34mbianyilin@bianyilin:/home/bianyilin\n\033[0m");
+    return 0;
+}
+
